569bd10e922ad92cf4da5a2ccc56e1db761c6e84
lshillman <lshillman@users.noreply.github.com>
2022-08-04 11:39:57 -0700

    add animal seeds

---
+++ seeds/animalSeed.js
@@ -0,0 +1,40 @@
+const { Animal } = require("../models/Animal");
+
+const animalData = [
+    {
+        farm_id: 1,
+        type: 'cow',
+        food_organic: 150,
+        food_manufactured: 100
+    },
+    {
+        farm_id: 2,
+        type: 'chicken',
+        food_organic: 150,
+        food_manufactured: 100
+    },
+    {
+        farm_id: 3,
+        type: 'pig',
+        food_organic: 150,
+        food_manufactured: 100
+    },
+    {
+        farm_id: 4,
+        type: 'squid',
+        food_organic: 150,
+        food_manufactured: 100
+    },
+    {
+        farm_id: 1,
+        type: 'cow',
+        food_organic: 150,
+        food_manufactured: 100
+    },
+];
+
+const animalSeeds = () => Animal.bulkCreate(animalData, {
+    individualHooks: true
+});
+
+module.exports = ( animalSeeds );
