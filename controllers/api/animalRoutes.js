11a7005b0f7f45266248d6bb1e5549442b921e64
lshillman <lshillman@users.noreply.github.com>
2022-08-04 15:44:54 -0700

    add get routes for all animals and a single animal. Add placeholder farm routes.

--- controllers/api/animalRoutes.js
+++ controllers/api/animalRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Animal } = require ('../../models');
+const {Animal} = require ('../../models');
 
 
 // get all animals
@@ -14,14 +14,27 @@ router.get('/', async (req, res) => {
     } catch(err) {
         res.status(500).json(err);
     }
-})
-
-
+});
 
 // get one animal
+
+router.get('/:id', async (req, res) => {
+    try {
+        const dbAnimalData = await Animal.findByPk(req.params.id);
+        if (!dbAnimalData) {
+            res.status(404).json({message: "No animals found"});
+            return;
+        }
+        res.status(200).json(dbAnimalData);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
 
 // post an animal
 
 // update an animal
 
 // delete an animal
+
+module.exports = router;
