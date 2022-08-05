87ba335106dc28ba0959b3f081f3f3ec8e838d46
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-08-05 15:37:44 -0400

    added try catch for homepages init load

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,2 +1,18 @@
 const router = require('express').Router();
 const { Animal, Farm, User } = require('../models');
+
+router.get('/', async (req,res) => {
+    try{
+        const posts = await Farm.findAll({
+            include: User,
+            attributes: {exclude: ['password']}    
+        });
+        const Farm = Animal.map((post) =>post.get({plain: true}));
+        res.render('homepage',{
+            Farm,
+            loggedIn: req.session.loggedIn
+        });
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
