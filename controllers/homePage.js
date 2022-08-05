2ef2529d6a2aa6ff97a00f62021148f34c574d92
marzec <gregmarzec02@gmail.com>
2022-08-05 17:03:34 -0400

    updated handlebars,models

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,18 +1,20 @@
-const router = require('express').Router();
-const { Farm, User } = require('../models');
+const router = require("express").Router();
+const { Farm, User } = require("../models");
 
-router.get('/', async (req,res) => {
-    try{
-        const posts = await Farm.findAll({
-            include: User,
-            attributes: {exclude: ['password']}    
-        });
-        const Farms = posts.map((post) =>post.get({plain: true}));
-        res.render('homepage',{
-            Farms,
-            loggedIn: req.session.loggedIn
-        });
-    }catch(err){
-        res.status(500).json(err);
-    }
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Farm.findAll({
+      include: User,
+      attributes: { exclude: ["password"] },
+    });
+    const Farms = posts.map((post) => post.get({ plain: true }));
+    res.render("homepage", {
+      Farms,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+module.exports = router;
