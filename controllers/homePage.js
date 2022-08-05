623d35c55e40c1f81911dbfd377365ebdc0f6c43
marzec <gregmarzec02@gmail.com>
2022-08-05 17:31:35 -0400

    homepagejs wip

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,16 +1,24 @@
-<<<<<<< HEAD
 const router = require("express").Router();
-const { Farm, User } = require("../models");
+const { Animal, Farm, User } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
     const posts = await Farm.findAll({
-      include: User,
-      attributes: { exclude: ["password"] },
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+        {
+          model: Animal,
+          attributes: ["output"],
+        },
+      ],
     });
-    const Farms = posts.map((post) => post.get({ plain: true }));
+    const farm = posts.map((farm) => farm.get({ plain: true }));
+    console.log(farm);
     res.render("homepage", {
-      Farms,
+      farm,
       loggedIn: req.session.loggedIn,
     });
   } catch (err) {
@@ -19,34 +27,3 @@ router.get("/", async (req, res) => {
 });
 
 module.exports = router;
-=======
-const router = require('express').Router();
-const { Animal, Farm, User } = require('../models');
-
-router.get('/', async (req,res) => {
-    try{
-        const posts = await Farm.findAll({
-            include:
-        [
-            {
-            model: User,
-            attributes: ["name"]
-            },
-            {
-            model: Animal,
-            attributes: ["output"]
-            }
-        ],
-        });
-        const Farm = posts.map((post) =>post.get({plain: true}));
-        res.render('homepage',{
-            Farm,
-            loggedIn: req.session.loggedIn
-        });
-    }catch(err){
-        res.status(500).json(err);
-    }
-});
-
-module.exports = router;
->>>>>>> 566169f79f33f1404bb4a37964d5f3a97499dcea
