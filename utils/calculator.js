5f9e5798a3a14d2dfec421672f6f055376aa8a0b
Matt F <fiaschettima@gmail.com>
2022-08-06 19:30:07 -0400

    add helper function to handlebars to calculate cost by time

--- utils/calculator.js
+++ utils/calculator.js
@@ -1,5 +1,24 @@
+// ex case given 
+// want to budget for 
+// 22 days and the cost per month is 275
 
 // TODO: make a function to format dollar amounts
-const calculator = () => {console.log('Hellow world!')}
+const calculator = (unit, quanity, monthlyCost) => {
+    let endBudget;
+    switch(unit){
+        case 'month':
+            endBudget = ((monthlyCost* quanity)).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+        break;
 
-module.exports = calculator;
+        case 'day':
+            endBudget= ((monthlyCost/30)*quanity).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+        break;
+        case 'year':
+            endBudget = ((monthlyCost*12)*quanity).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+        break;
+    }
+    console.log(endBudget)
+}
+calculator('month' ,22, 275)
+calculator('day' ,679, 200)
+calculator('year' ,8, 275)
