a267f5c92e1a8c90fb1d9a3db05ef386a765eae3
marzec <gregmarzec02@gmail.com>
2022-08-06 20:41:57 -0400

    push password conf to main

--- public/js/modals.js
+++ public/js/modals.js
@@ -1,8 +1,8 @@
-var logModal = document.getElementById("logModal");
+const logModal = document.getElementById("logModal");
 
-var logBtn = document.getElementById("logBtn");
+const logBtn = document.getElementById("logBtn");
 
-var logSpan = document.getElementById("logClose");
+const logSpan = document.getElementById("logClose");
 
 logBtn.onclick = function () {
   logModal.style.display = "block";
@@ -18,11 +18,11 @@ window.onclick = function (event) {
   }
 };
 
-var signModal = document.getElementById("signModal");
+const signModal = document.getElementById("signModal");
 
-var signBtn = document.getElementById("signBtn");
+const signBtn = document.getElementById("signBtn");
 
-var signSpan = document.getElementById("signClose");
+const signSpan = document.getElementById("signClose");
 
 signBtn.onclick = function () {
   signModal.style.display = "block";
@@ -38,24 +38,22 @@ window.onclick = function (event) {
   }
 };
 
-
-
 const loginFormHandler = async (event) => {
   event.preventDefault();
-  const name = document.querySelector('#username-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const name = document.querySelector("#username-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
   if (name && password) {
-    const response = await fetch('/api/user/login', {
-      method: 'POST',
+    const response = await fetch("/api/user/login", {
+      method: "POST",
       body: JSON.stringify({ name, password }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace("/");
     } else {
-      alert('Failed to log in.');
+      alert("Failed to log in.");
     }
   }
 };
@@ -63,28 +61,40 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#username-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const name = document.querySelector("#username-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+  //merge test?
+  const pass1 = document.querySelector("#password-signup").value.trim();
+  const pass2 = document.querySelector("#password-signup-conf").value.trim();
+
+  if (pass1 === pass2) {
+    console.log("password correct");
+    alert("Account Created Successfully!");
+  } else {
+    console.log("Password Incorrect");
+    alert("Account not created :(");
+    return;
+  }
 
   if (name && password) {
-    const response = await fetch('/api/user', {
-      method: 'POST',
+    const response = await fetch("/api/user", {
+      method: "POST",
       body: JSON.stringify({ name, password }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace("/");
     } else {
-      alert('Failed to sign up.');
+      alert("Failed to sign up.");
     }
   }
 };
 
 document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
 
 document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
