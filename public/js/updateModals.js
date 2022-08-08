2d276f2029fab06b76b1749bd9835f27e66126db
marzec <gregmarzec02@gmail.com>
2022-08-07 23:30:53 -0400

    added delete animal

--- public/js/updateModals.js
+++ public/js/updateModals.js
@@ -34,11 +34,6 @@ const updateAnimalFormHandler = async (event) => {
 
   const manCost = document.getElementById("edit-manufactured-cost").value;
 
-  console.log(
-    editAnimalID +
-    "TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST"
-  );
-
   if (animalType && organicCost && manCost) {
     const response = await fetch(`/api/animals/${editAnimalID}`, {
       method: "PUT",
@@ -61,6 +56,47 @@ const updateAnimalFormHandler = async (event) => {
   }
 };
 
+const deleteModal = document.getElementById("deleteModal");
+
+const deleteButton = document.getElementById("delAnimalBtn");
+
+const deleteSpan = document.getElementById("deleteModalClose");
+
+deleteButton.onclick = function () {
+  deleteModal.style.display = "block";
+};
+
+deleteSpan.onclick = function () {
+  deleteModal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == deleteModal) {
+    deleteModal.style.display = "none";
+  }
+};
+
+const deleteConfirmation = async (event) => {
+  event.preventDefault();
+  const delCancel = document.getElementById("deleteDenyBtn");
+  const delConfirm = document.getElementById("deleteConfBtn");
+  const delAnimalID = document.querySelector("#nameInfo").dataset.animalid;
+
+  delCancel.onclick = function () {
+    location.reload();
+  };
+  delConfirm.onclick = function () {
+    fetch(`/api/animals/${delAnimalID}`, {
+      method: "DELETE",
+    });
+    window.location.replace("/dashboard");
+  };
+};
+
 document
   .querySelector("#edit-form")
   .addEventListener("submit", updateAnimalFormHandler);
+
+document
+  .querySelector("#delete-form")
+  .addEventListener("submit", deleteConfirmation);
