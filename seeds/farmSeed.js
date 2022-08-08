df0dc0adb987af6453cca85e8024c4da3d32627d
marzec <gregmarzec02@gmail.com>
2022-08-08 15:40:04 -0400

    updated seeds for demo

--- seeds/farmSeed.js
+++ seeds/farmSeed.js
@@ -4,22 +4,27 @@ const farmData = [
     {
         name: 'Lukes Farm',
         description: 'Has chickens and cats',
-        user_id: 4
+        user_id: 1
     },
     {
         name: 'Matts Farm',
         description: 'Has cows',
-        user_id: 3
+        user_id: 2
     },
     {
         name: 'Sufis farm',
         description: 'tax haven',
-        user_id: 2
+        user_id: 3
     },
     {
         name: 'Gregs farm',
         description: 'Goat and squid farm',
-        user_id: 1
+        user_id: 4
+    },
+    {
+        name: 'Demo Farm',
+        description: 'Demo farm containing demo animals',
+        user_id: 5
     }
 ];
 const farms = () => Farm.bulkCreate(farmData);
