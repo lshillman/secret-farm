4bf08031f58e7f91f7eef3e12412a43513c1ea19
lshillman <lshillman@users.noreply.github.com>
2022-08-08 18:28:32 -0700

    fix farms page and dashboard styling

--- controllers/publicFarm.js
+++ controllers/publicFarm.js
@@ -4,6 +4,20 @@ const { Animal, Farm, User } = require("../models");
 //Get all Farms
 router.get("/", async (req, res) => {
   try {
+    const dbFarmData = await Farm.findAll({include: {
+      model: Animal,
+    }});
+    if (!dbFarmData) {
+      res.status(404).json({ message: "No farms found" });
+      return;
+    }
+    // console.log(dbFarmData);
+    const farmData = {dbFarmData: dbFarmData};
+    console.log(farmData);
+    res.render('directory',{
+      farmData,
+      logged_in: req.session.logged_in
+    });
   } catch (err) {
     res.status(500).json(err);
   }
